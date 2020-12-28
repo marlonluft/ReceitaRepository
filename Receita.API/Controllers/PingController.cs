0b1862f7747d5673394f1e8eaad930e192b5b559
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2020-12-28 14:48:31 -0300

    Criado controller específica para manipulação de receitas

--- Receita.API/Controllers/PingController.cs
+++ Receita.API/Controllers/PingController.cs
@@ -1,11 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Receita.Core.Entity;
-using Receita.Core.Service;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Receita.API.Controllers
 {
@@ -14,25 +8,15 @@ namespace Receita.API.Controllers
     public class PingController : ControllerBase
     {
         private readonly ILogger<PingController> _logger;
-        private readonly IReceitaService _receitaService;
 
-        public PingController(ILogger<PingController> logger, IReceitaService receitaService)
+        public PingController(ILogger<PingController> logger)
         {
             _logger = logger;
-            _receitaService = receitaService;
         }
 
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("Pong");
-        }
-
-        [HttpPost]
-        public IActionResult Gravar(ReceitaEntity entity)
-        {
-            _receitaService.GravarReceita(entity);
-
             return Ok("Pong");
         }
     }
---
+++ Receita.API/Controllers/ReceitaController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Receita.Core.Entity;
+using Receita.Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Receita.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReceitaController : Controller
+    {
+        private readonly ILogger<ReceitaController> _logger;
+        private readonly IReceitaService _receitaService;
+
+        public ReceitaController(ILogger<ReceitaController> logger, IReceitaService receitaService)
+        {
+            _logger = logger;
+            _receitaService = receitaService;
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            var lista = new List<ReceitaEntity>();
+
+            return Ok(lista);
+        }
+
+        [HttpPost]
+        public IActionResult Gravar(ReceitaEntity entity)
+        {
+            _receitaService.GravarReceita(entity);
+
+            return Ok("Pong");
+        }
+    }
+}
